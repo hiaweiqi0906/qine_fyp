3517ae03b8798388f6a3073a66cf652bf2f9cfd0
unknown <hiaweiqi@gmail.com>
2023-04-27 22:15:28 +0800

    Add log out and navbar

---
+++ components/navbar_index.php
@@ -0,0 +1,21 @@
+<header>
+          <div class="container">
+            <div class="logo">
+              <img src="../img/pjkukm.png" alt="Logo" />
+            </div>
+
+            <div class="links">
+              <ul>
+                <li><a href="#">Utama</a></li>
+                <li><a href="./soalanlazim.html">Soalan Lazim</a></li>
+                <li><a href="./login.php" class="btn">Log Masuk</a></li>
+              </ul>
+            </div>
+
+            <div class="overlay"></div>
+
+            <div class="hamburger-menu">
+              <div class="bar"></div>
+            </div>
+          </div>
+        </header>
